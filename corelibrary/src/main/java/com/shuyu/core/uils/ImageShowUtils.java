c3ce96b08b39faba87bfba374db3d29add0a2416
RayZhang <always_0122@163.com>
2017-03-07 16:38:28 +0800

    增加签名

--- corelibrary/src/main/java/com/shuyu/core/uils/ImageShowUtils.java
+++ corelibrary/src/main/java/com/shuyu/core/uils/ImageShowUtils.java
@@ -13,7 +13,7 @@ import com.shuyu.core.R;
 public class ImageShowUtils {
 
     public static void showImage(Context context, String url, ImageView imageView) {
-        Glide.with(context).load(url).placeholder(R.mipmap.ic_default_image).into(imageView);
+        Glide.with(context).load(url).placeholder(R.color.transparent).into(imageView);
     }
 
     public static void showImage(Context context, int url, ImageView imageView) {
